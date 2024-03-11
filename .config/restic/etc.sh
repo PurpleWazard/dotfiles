c285b43e33d11795136c213bd660f07f21bbe6ac
purple wazard <purplewazard@gmail.com>
2024-03-11 16:32:09 -0500

    remove restic scripts

--- .config/restic/etc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-iiip=99.168.110.242
-ressss=cur
-if curl ipinfo.io/ip | grep -q $iiip; then
-	echo "yes"
-	restic backup /etc --password-file /home/wazard/.config/restic/pass.txt -r sftp://root@10.0.1.23:22//backup/laptop/etc
-	echo "done"
-fi
-exit
--- .config/restic/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#restic init -r sftp://root@10.0.1.23://backup/laptop/etc --password-file /home/wazard/.config/restic/pass.txt
-#restic init -r sftp://root@10.0.1.23://backup/laptop/home --password-file /home/wazard/.config/restic/pass.txt
-restic init -r sftp://wazard@10.0.1.20://backup/laptop/etc --password-file /home/wazard/.config/restic/pass.txt
-restic init -r sftp://wazard@10.0.1.20://backup/laptop/home --password-file /home/wazard/.config/restic/pass.txt
