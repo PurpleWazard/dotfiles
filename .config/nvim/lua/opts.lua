a82f194140dfb9bef4522844ab42ac984b7c0613
purple wazard <purplewazard@gmail.com>
2024-03-29 21:48:53 -0500

    XDPH fix

--- .config/nvim/lua/opts.lua
+++ .config/nvim/lua/opts.lua
@@ -1,4 +1,5 @@
 vim.cmd("set expandtab")
+vim.cmd("set nowrap")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
@@ -7,3 +8,4 @@ vim.g.mapleader = " "
 vim.keymap.set('i', 'jk', '<Esc>')
 vim.keymap.set("n", "<leader>w", ":w<CR>")
 vim.keymap.set("n", "<leader>q", ":q<CR>")
+vim.o.termguicolors = true
