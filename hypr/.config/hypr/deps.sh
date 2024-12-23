8f4bb36f4944b4afc5e39aaba482fe25e272a064
purplewazard <purplewazard@proton.me>
2024-12-22 19:43:18 -0600

    change

---
+++ hypr/.config/hypr/deps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo pacman -S xorg-xwayland wofi wl-clipboard waybar qt6-wayland nwg-displays nwg-look hyprland hyprpaper hyprlock hypridle 
+
+yay -S hyprpolkitagent-git hyprutils-git xdg-desktop-portal-hyprland-git
