c7433b100e529cc589ffae1ef63571a2549a7f7d
Wazard <purplewazard@proton.me>
2025-09-21 12:02:22 -0500

    update

--- system-config/install.sh
+++ system-config/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 configapps=(nvim alacritty fish)
-apps="alacritty fish curl npm base base-devel vkd3d vlc neovim wine wine-mono winetricks vim vi unzip unrar ttf-hack-nerd sudo stow pipewire-alse pipewire-docs pipewire-pulse pipewire pipewire-ajck wireplumber networkmanager nautilus make luarocks kitty gvfs-smb git fakeroot cronie"
+apps="alacritty fish curl npm base base-devel vkd3d vlc neovim vim vi unzip unrar ttf-hack-nerd sudo stow pipewire-alsa pipewire-docs pipewire-pulse pipewire pipewire-jack wireplumber networkmanager nautilus make luarocks kitty gvfs-smb git fakeroot cronie"
 
 		
 if [[ "$1" == "-R" ]]; then
