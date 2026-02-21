a564006b54f6b2cabe6ab15d00cd82b5614646c6
Wazard <purplewazard@proton.me>
2026-02-21 12:48:45 -0600

    neovim debug update

--- configs/nvim/.config/nvim/lua/plugins/debug.lua
+++ configs/nvim/.config/nvim/lua/plugins/debug.lua
@@ -1,4 +1,3 @@
-
 return {
   {
     "mfussenegger/nvim-dap",
@@ -16,6 +15,10 @@ return {
 
       vim.keymap.set("n", "<space>db", dap.toggle_breakpoint)
       vim.keymap.set("n", "<space>dc", dap.continue)
+      vim.keymap.set('n', '<leader>do', dap.step_over)
+      vim.keymap.set('n', '<leader>di', dap.step_into)
+      vim.keymap.set('n', '<leader>du', dap.step_out)
+      vim.keymap.set('n', '<Leader>dr', dap.repl.open)
 
       dap.listeners.before.attach.dapui_config = function()
         ui.open()
@@ -58,29 +61,6 @@ return {
           cwd = "${workspaceFolder}",
           stopAtBeginningOfMainSubprogram = false,
         },
-        {
-          name = "Select and attach to process",
-          type = "gdb",
-          request = "attach",
-          program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-          end,
-          pid = function()
-            local name = vim.fn.input('Executable name (filter): ')
-            return require("dap.utils").pick_process({ filter = name })
-          end,
-          cwd = '${workspaceFolder}'
-        },
-        {
-          name = 'Attach to gdbserver :1234',
-          type = 'gdb',
-          request = 'attach',
-          target = 'localhost:1234',
-          program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-          end,
-          cwd = '${workspaceFolder}'
-        }
       }
 
 
